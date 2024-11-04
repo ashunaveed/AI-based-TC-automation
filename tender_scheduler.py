33889ed91f34a336be9966146f09868ec06292b4
Md Naveed Ashfaq <43320599+ashunaveed@users.noreply.github.com>
2024-11-05 00:23:08 +0530

    Update tender_scheduler.py

--- tender_scheduler.py
+++ tender_scheduler.py
@@ -137,21 +137,108 @@ def main():
             use_AI =0
         if event == sg.WINDOW_CLOSED:
             break
-        elif event == "-LOA1-":
-            process_file(work_folders, use_AI, 0, final_schedule.main, 'xlsx')
+       elif event == "-LOA1-":
+            #try:
+            processed_data1 = final_schedule.main(use_AI) 
+            toime = datetime.datetime.now().strftime('%H_%M_%S of %d-%m')
+            file_name = f"temporary_file_{toime}.xlsx"
+            folder_path = os.path.join(current_directory,work_folders[0])
+            path1 = os.path.join(folder_path, file_name)
+            #print('Path1 taken')
+            processed_data1.to_excel(path1, engine = 'openpyxl')
+            file_name = f"main_works_comparision_file_{toime}.xlsx"
+            patha = os.path.join(folder_path, file_name)
+            workbook = openpyxl.load_workbook(path1)
+            for sheet_name in workbook.sheetnames:
+                apply_format_to_sheet(workbook[sheet_name])
+            # Save the workbook after formatting
+            workbook.save(patha)
+            sg.popup('The Excel sheet named primary_work_bid_comparision is generated in ',work_folders[0])
+            #except:
+            #    sg.popup('Please select valid HTML/PDF files')
         elif event == "-MIN_LOA-":
-            process_file(work_folders, use_AI, 1, Sub_works_references.main, 'xlsx')
+            try:
+                processed_data2 = Sub_works_references.main(use_AI)
+                toime = datetime.datetime.now().strftime('%H_%M_%S of %d-%m')
+                file_name = f"temporary_file_{toime}.xlsx"
+                folder_path = os.path.join(current_directory,work_folders[1])
+                path2 = os.path.join(folder_path, file_name)
+                excel_writer = pd.ExcelWriter(path2, engine="openpyxl")
+                # Write each DataFrame to a separate sheet in the Excel file
+                for idx, df in enumerate(processed_data2, start=1):
+                    sheet_name = f"Subwork_{idx}"  # Change the sheet name as needed
+                    df.to_excel(excel_writer, sheet_name=sheet_name, index=False)
+                excel_writer.book.save(path2)
+                workbook = openpyxl.load_workbook(path2)
+                for sheet_name in workbook.sheetnames:
+                    apply_format_to_sheet(workbook[sheet_name])
+                file_name = f"sub_works_comparision_file_{toime}.xlsx"
+                pathb = os.path.join(folder_path, file_name)
+                workbook.save(pathb)
+                sg.popup('The excel sheet is generated with name sub_work_bid_comparision on the desktop')
+            except:
+                sg.popup('Please select valid sub work HTML/PDF files')
         elif event == "-FINAL_main_work-":
-            filepath = values["-EXCEL1-"]
-            if filepath:
-                process_file(work_folders, filepath, 2, DOCX_writing.main, 'xlsx')
+            try:
+                filepath = values["-EXCEL1-"]
+                if filepath:
+                    processed_data3 = DOCX_writing.main(filepath)
+                    toime = datetime.datetime.now().strftime('%H_%M_%S of %d-%m')
+                    file_name = f"Final_main_Schedule_report_{toime}.xlsx"
+                    folder_path = os.path.join(current_directory,work_folders[2])
+                    path3 = os.path.join(folder_path, file_name)
+                    processed_data3.to_excel(path3, engine = 'openpyxl')
+                    workbook = openpyxl.load_workbook(path3)
+                    sheet = workbook.active
+                    cols = list(processed_data3.columns)
+                    for col_num, col in enumerate(cols, start=1):
+                        start_row = None
+                        for row_num in range(2, sheet.max_row + 2):
+                            if row_num <= sheet.max_row and sheet.cell(row=row_num, column=col_num).value == sheet.cell(row=row_num-1, column=col_num).value:
+                                if start_row is None:
+                                    start_row = row_num - 1
+                            else:
+                                if start_row:
+                                    sheet.merge_cells(start_row=start_row, start_column=col_num, end_row=row_num-1, end_column=col_num)
+                                    start_row = None
+                    for row in sheet.iter_rows():
+                        for cell in row:
+                            cell.alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
+                    workbook.save(path3)
+                    sg.popup('The excel sheet is generated with name Final_Schedule_report on the desktop')
+            except:
+                sg.popup('Please select correct main work Excelsheet files')
         elif event == "-FINAL_sub_work-":
-            filepath = values["-EXCEL2-"]
-            if filepath:
-                process_file(work_folders, filepath, 3, Sub_works_writing.main, 'xlsx')
-
+            try:
+                filepath = values["-EXCEL2-"]
+                if filepath:
+                    toime = datetime.datetime.now().strftime('%H_%M_%S of %d-%m')
+                    file_name = f"Final_sub_Schedule_report_{toime}.xlsx"
+                    folder_path = os.path.join(current_directory,work_folders[3])
+                    path4 = os.path.join(folder_path, file_name)
+                    processed_data4 = Sub_works_writing.main(filepath)
+                    processed_data4.to_excel(path4, engine = 'xlsxwriter')
+                    workbook = openpyxl.load_workbook(path4)
+                    sheet = workbook.active
+                    cols = list(processed_data4.columns)
+                    for col_num, col in enumerate(cols, start=1):
+                        start_row = None
+                        for row_num in range(2, sheet.max_row + 2):  # Adjusting the loop to consider one more iteration
+                            if row_num <= sheet.max_row and sheet.cell(row=row_num, column=col_num).value == sheet.cell(row=row_num-1, column=col_num).value:
+                                if start_row is None:
+                                    start_row = row_num - 1
+                            else:
+                                if start_row:
+                                    sheet.merge_cells(start_row=start_row, start_column=col_num, end_row=row_num-1, end_column=col_num)
+                                    start_row = None
+                    for row in sheet.iter_rows():
+                        for cell in row:
+                            cell.alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
+                    workbook.save(path4)
+                    sg.popup('The excel sheet is generated with name Final_Sub_schedule_report on the desktop')
+            except:
+                sg.popup('Please select correct subwork Excelsheet files')
     window.close()
-
-
 if __name__ == "__main__":
     main()
+        
